626825971f6ae0af7de3eec34f3700a66cad8a7f
Dimitri Rosenberg <di@radiant.fm>
2014-07-23 09:41:31 +0400

    adding photo uploader

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,9 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :grid_fs
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore.pluralize}/#{model.id}/#{mounted_as.to_s.pluralize}/"
+  end
+end
