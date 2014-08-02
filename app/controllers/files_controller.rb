e41d1ce2cdf17792802c4d70128cfcbd362ed6ef
Dimitri Rosenberg <di@radiant.fm>
2014-08-02 13:43:21 +0400

    adding cors to files

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,4 +1,14 @@
 class FilesController < ActionController::Base
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers["Access-Control-Allow-Origin"]  = request.env["HTTP_ORIGIN"]
+    headers["Access-Control-Allow-Methods"] = "GET"
+    headers["Access-Control-Max-Age"]       = "1728000"
+  end
+
   ##
   # Use CDN for load balancing, high availability and performance.
   #
